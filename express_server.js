ef73453eb4be43583b48c51cc8e052401e958e05
Shujie Ma <shujie1st@gmail.com>
2023-03-04 14:18:49 -0500

    Add DELETE to remove short URL from database

--- express_server.js
+++ express_server.js
@@ -59,6 +59,11 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect("/urls");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
