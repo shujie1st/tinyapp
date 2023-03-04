926c243452596c653e44e886018e27dfda70c9e8
Shujie Ma <shujie1st@gmail.com>
2023-03-04 16:27:59 -0500

    Add post route to update long URL

--- express_server.js
+++ express_server.js
@@ -64,6 +64,11 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect("/urls");
 });
 
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id] = req.body.newURL;
+  res.redirect("/urls")
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
